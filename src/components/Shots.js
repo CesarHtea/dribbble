59eac33e00a3949b544419e76b2e709f381bf974
Yair <hello@agzeri.com>
2018-03-15 11:41:43 -0600

    [Shots.js] Add  function with correct markup.

--- src/components/Shots.js
+++ src/components/Shots.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 
-class Shots extends Component {}
+class Shots extends Component {
+  render() {
+    return (
+      <section>
+        <div className='container'>
+          <div className='shots'>
+            return <Shot />
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Shots;
